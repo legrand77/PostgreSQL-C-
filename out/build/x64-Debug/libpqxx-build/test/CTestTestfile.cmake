e2515f7f7e4f3d977f0e954304f0d9497aa33bec
legrand77 <gromovaleksandr813@gmail.com>
2025-02-18 13:07:38 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Учеба/Нетология С++/Базы данных/Дом_раб_5/libpqxx/test
-# Build directory: C:/Учеба/Нетология С++/Базы данных/Дом_раб_5/out/build/x64-Debug/libpqxx-build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "C:/Учеба/Нетология С++/Базы данных/Дом_раб_5/out/build/x64-Debug/libpqxx-build/test/runner.exe")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "C:/Учеба/Нетология С++/Базы данных/Дом_раб_5/libpqxx/test/CMakeLists.txt;12;add_test;C:/Учеба/Нетология С++/Базы данных/Дом_раб_5/libpqxx/test/CMakeLists.txt;0;")
